37521198a539dbf70ea0202ebf2d74f91bc57e87
Cesar Guevara Cabrera <sarcansino@gmail.com>
2021-07-05 23:10:09 -0500

    terminada la primera card con estilos y todo

--- scripts-proyectos.js
+++ scripts-proyectos.js
@@ -1,4 +1,23 @@
 let checkbox = document.querySelector('.sw_dark_mode_input')
+var Andi
+
+checkbox? Andi= 1 : Andi = 0;
+
+if (Andi==1) {
+    console.log("Andi es 1");
+} else {
+    console.log("Andi es 0");
+}
+
+setTimeout(function(){
+    if (checkbox.checked==true) {
+        document.body.classList.remove('light-mode')
+        document.body.classList.add('dark-mode')
+    } else {
+        document.body.classList.remove('dark-mode')
+        document.body.classList.add('light-mode')
+    }
+},0)
 
 checkbox.addEventListener('change', function(event){
     console.log(this.checked);
