a97b7ef8ed1027435307f76362bcf16f4a98e697
César Guevara Cabrera <sarcansino@gmail.com>
2023-11-08 19:04:43 -0500

    Funcionamiento de boton "Proyectos"

--- app/page.jsx
+++ app/page.jsx
@@ -4,16 +4,22 @@ import HeroImage from "../assets/cesar.jpg"
 import LinkedinIcon from "../assets/linkedin.svg"
 import GitHubIcon from "../assets/github.svg"
 import WhatsappIcon from "../assets/wsp.svg"
-
+import "./page.css"
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="px-7 mt-6 flex flex-col items-center">
+      
+      
       {/* // Greeting */}
       <div className="flex flex-col w-full">
         <span className="text-sm">Hola 👋, soy</span>
         <h1 className="text-3xl font-bold">César Guevara</h1>
       </div>
+
+
+    
       {/* // Hero */}
       <div className="relative w-[275px] mt-5">
         <Image
@@ -25,12 +31,20 @@ export default function Home() {
         <div className="w-24 h-6 bg-yellow-500 absolute top-14 -left-2 -z-10"></div>
         <div className="w-24 h-6 bg-black absolute bottom-6 -right-1 -z-10"></div>
       </div>
+
+
+
+
       {/* // Career */}
       <div className="flex justify-start w-full gap-1 text-base mt-[28px]">
         <h2 className="text-sky-400 font-bold">Frontend Developer</h2>
         <span className="text-yellow-500 font-bold">/</span>
         <h2 className="text-green-600 font-bold">Electronic Engineer</h2>
       </div>
+
+
+
+
       {/* // CallToAction */}
       <div className="mt-[10px]">
         <p className="text-sm text-left">
@@ -39,11 +53,16 @@ export default function Home() {
           <br />
           Te invito a conocer mis proyectos.
         </p>
-        <button className="text-sm bg-yellow-500 py-[12px] px-[7px] flex gap-2 justify-center items-center mt-[21px] w-[137px]">
+        <Link href="/proyectos" className="text-sm bg-yellow-500 py-[12px] px-[7px] flex gap-2 justify-center items-center mt-[21px] w-[137px] action-button">
           Proyectos
           <MdOutlineWeb size="24" color="#493E00" />
-        </button>
+        </Link>
       </div>
+
+
+
+
+
       {/* // Buttons */}
       <div className="flex justify-center w-full gap-10 mt-[44px] mb-10">
         <a href="https://www.linkedin.com/in/domakedev/"
